963cf28fe2815a02792ef17ecf72a8fff722b00e
Mihail Gerasimov <mihailgerasimov86@gmail.com>
2021-10-05 13:58:47 +0400

    'f'

---
+++ классификаия-типов-данных.js
@@ -0,0 +1,18 @@
+"use strict";
+
+let number = 4.6;
+
+console.log(4 / 0);
+console.log(-4 / 0);
+
+console.log('string' * 9);
+
+const persone = "alex";
+
+const bool = true;
+const boolL = false;
+
+console.log(something);
+
+let und;
+console.log(und);
