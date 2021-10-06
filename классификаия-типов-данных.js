e6009f0ab6fc4f2a9fd37f4612182bcd4637a37d
Mihail <mihailgerasimov86@gmail.com>
2021-10-07 01:33:28 +0400

    's'

--- классификаия-типов-данных.js
+++ классификаия-типов-данных.js
@@ -1,18 +1,28 @@
 "use strict";
-
-let number = 4.6;
+let number = 4;
 
 console.log(4 / 0);
 console.log(-4 / 0);
-
 console.log('string' * 9);
 
-const persone = "alex";
-
+const person = "alex";
 const bool = true;
-const boolL = false;
+const booLL = false;
 
-console.log(something);
+// console.log(sumthing);
 
 let und;
 console.log(und);
+
+const obj = {
+    name: "misha",
+    age: 24,
+    isMarried: false
+};
+
+console.log(obj.age);
+console.log(obj["name"]);
+
+let arr = ['./.jshint', 'yellow.jpg'];
+
+console.log(arr[0]);
---
+++ общение-с-пользователем.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// alert('hello');
+
+// const result = confirm('хочешь кушать?');
+// console.log(result);
+
+// const answer = prompt("Вам есть 18?", "");
+// const answer = prompt("Вам есть 18?", "");
+// console.log(typeof (answer), answer);
+// console.log(answer + 5);
+
+// const answers = [];
+// answers[0] = prompt("как ваше имя?", "");
+// answers[1] = prompt("как ваше фамилия?", "");
+// answers[2] = prompt("как ваше отчество?", "");
+// document.write(answers);
+// console.log(typeof (answers));
+
+const myName = prompt('как тебя зовут?', '');
+console.log(myName);
